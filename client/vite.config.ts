8247f1f12fd22ab7e115988052dad4a380a3e1f1
Gaurav Tendolkar <codetendolkar@gmail.com>
2026-03-30 18:51:25 -0400

    v0.1

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 5173,
@@ -15,11 +14,11 @@ export default defineConfig(({ mode }) => ({
       '/settings': 'http://localhost:3120',
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
   },
-}));
+});
